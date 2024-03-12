13f6a564dc4e8137ea9da7bd79e5552eb93160e6
almustapha.tukur <atumar4031@gmail.com>
2024-03-12 22:57:14 +0100

    base response

--- src/main/java/com/oasis/TaskManagementApplication/exception/AppExceptionHandler.java
+++ src/main/java/com/oasis/TaskManagementApplication/exception/AppExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.oasis.TaskManagementApplication.exception;
 
-import com.oasis.TaskManagementApplication.dto.res.APIResponse;
 import com.oasis.TaskManagementApplication.dto.res.BaseResponse;
 import com.oasis.TaskManagementApplication.dto.res.ExceptionResponse;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -49,11 +48,11 @@ public class AppExceptionHandler {
     @ExceptionHandler({MethodArgumentTypeMismatchException.class})
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<APIResponse> resolveException(MethodArgumentTypeMismatchException ex) {
+    public ResponseEntity<BaseResponse<?>> resolveException(MethodArgumentTypeMismatchException ex) {
         String message = "Parameter '" + ex.getParameter().getParameterName() + "' must be '"
                 + Objects.requireNonNull(ex.getRequiredType()).getSimpleName() + "'";
-        APIResponse apiResponse = new APIResponse(Boolean.FALSE, message);
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        BaseResponse<?> baseResponse = new BaseResponse<>(Boolean.FALSE, message, null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
     // OBJECTS REQUEST FIELDS EXCEPTIONS END
     // CUSTOM APPLICATION EXCEPTIONS START
@@ -61,37 +60,35 @@ public class AppExceptionHandler {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<BaseResponse<?>> resolveException(BadRequestException exception) {
-       String message = exception.getMessage();
-        BaseResponse<?> response = new BaseResponse<>(Boolean.FALSE, message, null);
+        BaseResponse<?> response = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<APIResponse> resolveException(UnauthorizedException exception) {
-        APIResponse apiResponse = exception.getApiResponse();
-
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<BaseResponse<?>> resolveException(UnauthorizedException exception) {
+        BaseResponse<?> baseResponse =  new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<APIResponse> resolveException(NoResourceFoundException exception) {
-        APIResponse apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
-        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    public ResponseEntity<BaseResponse<?>> resolveException(NoResourceFoundException exception) {
+      BaseResponse<?> baseResponse =  new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.NOT_FOUND);
     }
     // CUSTOM APPLICATION EXCEPTIONS END
     // HTTP METHODS EXCEPTIONS START
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
     @ResponseBody
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    public ResponseEntity<APIResponse> resolveException(HttpRequestMethodNotSupportedException ex) {
+    public ResponseEntity<BaseResponse<?>> resolveException(HttpRequestMethodNotSupportedException ex) {
         String message = "Request method '" + ex.getMethod() + "' not supported. List of all supported methods - "
                 + ex.getSupportedHttpMethods();
-        APIResponse apiResponse = new APIResponse(Boolean.FALSE, message);
-        return new ResponseEntity<>(apiResponse, HttpStatus.METHOD_NOT_ALLOWED);
+        BaseResponse<?> baseResponse = new BaseResponse<>(Boolean.FALSE, message, null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @ExceptionHandler({HttpMessageNotReadableException.class})
@@ -109,67 +106,67 @@ public class AppExceptionHandler {
     @ExceptionHandler(AuthenticationException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<APIResponse> resolveAuthenticationException(AuthenticationException exception) {
+    public ResponseEntity<BaseResponse<?>> resolveAuthenticationException(AuthenticationException exception) {
 
-        APIResponse apiResponse = null;
+        BaseResponse<?> baseResponse = null;
 
         if (exception instanceof BadCredentialsException) {
-            apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
+            baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
         }
 
         if (exception instanceof DisabledException) {
-            apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
+            baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
         }
 
         if (exception instanceof AccountExpiredException) {
-            apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
+            baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
         }
 
         if (exception instanceof CredentialsExpiredException) {
-            apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
+            baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
         }
 
 
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(LockedException.class)
     @ResponseBody
-    public ResponseEntity<APIResponse> resolveLockedException(LockedException exception) {
-        APIResponse apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<BaseResponse<?>> resolveLockedException(LockedException exception) {
+        BaseResponse<?> baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
     // SPRING SECURITY USER DETAILS EXCEPTIONS END
     // JWT EXCEPTIONS START
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(JwtException.class)
-    public final ResponseEntity<APIResponse> handleJwtException(JwtException ex) {
-        APIResponse apiResponse = null;
+    public final ResponseEntity<BaseResponse<?>> handleJwtException(JwtException ex) {
+        BaseResponse<?> baseResponse = null;
 
         if (ex instanceof SignatureException) {
-            apiResponse = new APIResponse(Boolean.FALSE, "Invalid JWT signature");
+            baseResponse = new BaseResponse<>(Boolean.FALSE, "Invalid JWT signature", null);
         }
         if (ex instanceof MalformedJwtException) {
-            apiResponse = new APIResponse(Boolean.FALSE, "Invalid JWT token");
+            baseResponse = new BaseResponse<>(Boolean.FALSE, "Invalid JWT token", null);
         }
 
         if (ex instanceof UnsupportedJwtException) {
-            apiResponse = new APIResponse(Boolean.FALSE, "Unsupported JWT token");
+            baseResponse = new BaseResponse<>(Boolean.FALSE, "Unsupported JWT token",null);
         }
 
         if (ex instanceof ExpiredJwtException) {
-            apiResponse = new APIResponse(Boolean.FALSE, "Expired JWT token");
+            baseResponse = new BaseResponse<>(Boolean.FALSE, "Expired JWT token", null);
         }
 
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
 
     // JWT EXCEPTIONS END
     // APPLICATION FALLBACK EXCEPTIONS HANDLER
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<APIResponse> resolveExceptions(Exception exception) {
+    public ResponseEntity<BaseResponse<?>> resolveExceptions(Exception exception) {
         log.info("FALLBACK ERROR HERE ::-> ", exception.fillInStackTrace());
-        APIResponse apiResponse = new APIResponse(Boolean.FALSE, exception.getMessage());
-        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        BaseResponse<?> baseResponse = new BaseResponse<>(Boolean.FALSE, exception.getMessage(), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
     }
 }
